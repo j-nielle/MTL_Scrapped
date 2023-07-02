59496fc56cad44996f90e263c388b4674506b1b6
janielledagondon <toonw48@gmail.com>
2023-07-03 06:53:12 +0800

    hays

--- public/js/notifs.js
+++ public/js/notifs.js
@@ -1,61 +1,43 @@
+// Load the data from the server using Server-Sent Events
 document.addEventListener('DOMContentLoaded', function () {
-    var eventSource = new EventSource('/sse-request');
-    var table = document.getElementById('sse-data');
-    var renderTimeout;
+    const eventSource = new EventSource('/sse-request');
+    const container = document.getElementById('notifs-container');
+    const body = document.getElementById('notifs-tbody');
+    const maxRows = 10; // Set the maximum number of rows to display
+
+    let renderTimeout;
 
     eventSource.onmessage = function (event) {
-        var responseData = JSON.parse(event.data);
-    
+        const responseData = JSON.parse(event.data);
+        console.log(responseData);
+
         if (responseData.length === 0) {
             return; // Skip the update if responseData is empty
         }
-    
+
         if (renderTimeout) {
             clearTimeout(renderTimeout); // Clear previous timeout to limit rendering frequency
         }
-    
-        renderTimeout = setTimeout(function () {
-            var fragment = document.createDocumentFragment();
-            var rowsHtml = '';
-    
-            responseData.forEach(function (item) {
-                var row = document.createElement('tr');
-                row.className = 'text-gray-900 border-b border-gray-300';
-                
-                var contactNumCell = document.createElement('td');
-                contactNumCell.className = 'px-4 py-2';
-                contactNumCell.textContent = item.contactNum;
-                
-                var requestTypeCell = document.createElement('td');
-                requestTypeCell.className = 'px-4 py-2';
-                requestTypeCell.textContent = item.requestType;
-                
-                row.appendChild(contactNumCell);
-                row.appendChild(requestTypeCell);
-                fragment.appendChild(row);
-            });
-    
-            table.innerHTML = ''; // Clear the existing table
-            table.appendChild(fragment); // Append all rows at once
-    
-            var tableHtml = `
-                <thead class="font-bold leading-6 tracking-widest text-left text-white bg-gray-800 border-b border-gray-200">
-                    <tr>
-                        <th class="p-4">
-                            Contact Number
-                        </th>
-                        <th class="p-4">
-                            Request Type
-                        </th>
-                        <th class="p-4">
-                            Show/Hide
-                        </th>
+
+        renderTimeout = setTimeout(() => {
+            const rowsHtml = responseData
+                .slice(0, maxRows) // Limit the number of rows to maxRows
+                .map((item) => `
+                    <tr class="text-gray-900 border-b border-gray-300">
+                        <td class="px-4 py-2">${item.contactNum}</td>
+                        <td class="px-4 py-2">${item.requestType}</td>
                     </tr>
-                </thead>
-            `;
-    
-            table.insertAdjacentHTML('afterbegin', tableHtml); // Prepend the table header
-    
-        }, 1000); // Set a delay of 100 milliseconds before rendering the updated data
-    };    
+                `)
+                .join('');
+
+            body.innerHTML = rowsHtml;
+
+            // Add a scrollbar if the number of rows exceeds the maximum limit
+            if (responseData.length > maxRows) {
+                container.style.overflowY = "scroll";
+            } else {
+                container.style.overflowY = "auto";
+            }
+        }, 1000); // Set a delay of 1000 milliseconds before rendering the updated data
+    };
 });
