9c397a5a8cb3dae40b1761bfcd4023ec5e945476
janielledagondon <toonw48@gmail.com>
2023-07-02 20:25:30 +0800

    documentFragment usage

--- public/js/notifs.js
+++ public/js/notifs.js
@@ -5,24 +5,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
     eventSource.onmessage = function (event) {
         var responseData = JSON.parse(event.data);
-
+    
         if (responseData.length === 0) {
             return; // Skip the update if responseData is empty
         }
-
+    
         if (renderTimeout) {
             clearTimeout(renderTimeout); // Clear previous timeout to limit rendering frequency
         }
-
+    
         renderTimeout = setTimeout(function () {
-            var rowsHtml = responseData.map(function (item) {
-                return `<tr class="text-gray-900 border-b border-gray-300">
-                            <td class="px-4 py-2">${item.contactNum}</td>
-                            <td class="px-4 py-2">${item.requestType}</td>
-                        </tr>`;
-            }).join('');
-
-            table.innerHTML = `
+            var fragment = document.createDocumentFragment();
+            var rowsHtml = '';
+    
+            responseData.forEach(function (item) {
+                var row = document.createElement('tr');
+                row.className = 'text-gray-900 border-b border-gray-300';
+                
+                var contactNumCell = document.createElement('td');
+                contactNumCell.className = 'px-4 py-2';
+                contactNumCell.textContent = item.contactNum;
+                
+                var requestTypeCell = document.createElement('td');
+                requestTypeCell.className = 'px-4 py-2';
+                requestTypeCell.textContent = item.requestType;
+                
+                row.appendChild(contactNumCell);
+                row.appendChild(requestTypeCell);
+                fragment.appendChild(row);
+            });
+    
+            table.innerHTML = ''; // Clear the existing table
+            table.appendChild(fragment); // Append all rows at once
+    
+            var tableHtml = `
                 <thead class="font-bold leading-6 tracking-widest text-left text-white bg-gray-800 border-b border-gray-200">
                     <tr>
                         <th class="p-4">
@@ -36,8 +52,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         </th>
                     </tr>
                 </thead>
-                <tbody">${rowsHtml}</tbody>
             `;
+    
+            table.insertAdjacentHTML('afterbegin', tableHtml); // Prepend the table header
+    
         }, 100); // Set a delay of 100 milliseconds before rendering the updated data
-    };
+    };    
 });
