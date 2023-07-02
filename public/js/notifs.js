5d04bec01ae34543f742261a9a7eaab3d8fe6411
janielledagondon <toonw48@gmail.com>
2023-07-02 19:04:30 +0800

    RT notifs table styled v1

--- public/js/notifs.js
+++ public/js/notifs.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var eventSource = new EventSource('/sse-request');
-    var tableBody = document.getElementById('sse-data');
+    var table = document.getElementById('sse-data');
     var renderTimeout;
 
     eventSource.onmessage = function (event) {
         var responseData = JSON.parse(event.data);
 
         if (responseData.length === 0) {
-        return; // Skip the update if responseData is empty
+            return; // Skip the update if responseData is empty
         }
 
         if (renderTimeout) {
@@ -16,10 +16,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
         renderTimeout = setTimeout(function () {
             var rowsHtml = responseData.map(function (item) {
-                return `<tr><td>${item.contactNum}</td><td>${item.requestType}</td></tr>`;
+                return `<tr class="text-gray-900 border-b border-gray-300">
+                            <td class="px-4 py-2">${item.contactNum}</td>
+                            <td class="px-4 py-2">${item.requestType}</td>
+                        </tr>`;
             }).join('');
 
-            tableBody.innerHTML = rowsHtml;
+            table.innerHTML = `
+                <thead class="font-bold leading-6 tracking-widest text-left text-white bg-gray-800 border-b border-gray-200">
+                    <tr>
+                        <th class="p-4">
+                            Contact Number
+                        </th>
+                        <th class="p-4">
+                            Request Type
+                        </th>
+                        <th class="p-4">
+                            Show/Hide
+                        </th>
+                    </tr>
+                </thead>
+                <tbody">${rowsHtml}</tbody>
+            `;
         }, 100); // Set a delay of 100 milliseconds before rendering the updated data
     };
 });
--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -1,8 +1,16 @@
 <x-app-layout>
     <script src="{{ asset('js/notifs.js') }}"></script>
 
-    <table id="sse-data">
-    </table>
+    <div class="p-4 text-gray-900 dark:text-gray-100">
+        <div class="flex flex-row">
+            <div class="w-full p-4 mb-4">
+                <div class="max-w-screen-md md:max-w-screen-sm">
+                    <table class="w-auto table-fixed sm:table-auto shadow-lg bg-white" id="sse-data">
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
     {{-- 
     <div class="p-4 text-gray-900 dark:text-gray-100">
         <div class="flex flex-row">
