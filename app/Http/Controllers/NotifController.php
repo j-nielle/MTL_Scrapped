f94e73c9b1ab146058a70153ebe3fb085eb29851
janielledagondon <toonw48@gmail.com>
2023-07-03 17:48:38 +0800

    chuchu

--- app/Http/Controllers/NotifController.php
+++ app/Http/Controllers/NotifController.php
@@ -12,11 +12,6 @@ class NotifController extends Controller
 {
     public function index()
     {
-        $requests = Requests::join('contact', 'contact.contactID', '=', 'request.contactID')
-                            ->join('request_type', 'request_type.requestTypeID', '=', 'request.requestTypeID')
-                            ->select('contact.contactNum', 'request_type.requestType')
-                            ->orderBy('requestID','desc')->get();
-
-        return view('notifications',['requests' => $requests]);
+        return view('notifications');
     }
 }
