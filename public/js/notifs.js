f7db1e64eda42c9e0150630df86e57eb88d6cc1e
janielledagondon <toonw48@gmail.com>
2023-07-03 13:13:15 +0800

    notifs v1.2

--- public/js/notifs.js
+++ public/js/notifs.js
@@ -1,42 +1,48 @@
-document.addEventListener('DOMContentLoaded', function () {
+function handleSSEUpdates() {
     const tbody = document.getElementById("notifs-tbody");
-    const eventSource = new EventSource("/sse-request");
+    let previousCreatedAt;
     let renderTimeout;
 
-    let previousCreatedAt; // Variable to store the previous eventData.created_at value
-
-    eventSource.onmessage = function (event) {
-        const eventData = JSON.parse(event.data);
+    function renderData(eventData) {
         const maxRows = eventData.length;
 
+        const rowsHtml = eventData
+            .slice(0, maxRows)
+            .map((item) => `
+                <tr class="text-gray-900 border-b border-gray-300">
+                    <td class="px-4 py-2">${item.Phone}</td>
+                    <td class="px-4 py-2">${item.RequestType}</td>
+                </tr>
+            `)
+            .join('');
+
+        tbody.innerHTML = rowsHtml;
+    }
+
+    function handleSSEMessage(event) {
+        const eventData = JSON.parse(event.data);
+
         if (eventData.length === 0) {
-            return; // Skip the update if eventData is empty
+            return;
         }
 
         const latestCreatedAt = eventData[0].created_at;
 
         if (latestCreatedAt === previousCreatedAt) {
             console.log('same timestamp');
-            return; // Skip the update if the latest created_at is the same as the previous one
-        }else {
+            return;
+        } else {
             previousCreatedAt = latestCreatedAt;
             console.log('new timestamp');
         }
 
-        clearTimeout(renderTimeout); // Clear previous timeout to limit rendering frequency
-
+        clearTimeout(renderTimeout);
         renderTimeout = setTimeout(() => {
-            const rowsHtml = eventData
-                .slice(0, maxRows) // Limit the number of rows to maxRows
-                .map((item) => `
-        <tr class="text-gray-900 border-b border-gray-300">
-          <td class="px-4 py-2">${item.Phone}</td>
-          <td class="px-4 py-2">${item.RequestType}</td>
-        </tr>
-      `)
-                .join('');
+            renderData(eventData);
+        }, 100);
+    }
+    const eventSource = new EventSource("/sse-request");
+    eventSource.onmessage = handleSSEMessage;
+}
 
-            tbody.innerHTML = rowsHtml;
-        }, 100); // Set a delay of 1000 milliseconds before rendering the updated data
-    };
-});
+document.addEventListener('DOMContentLoaded', handleSSEUpdates);
