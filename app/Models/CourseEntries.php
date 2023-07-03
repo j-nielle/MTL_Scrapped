fb5ebbaa5dd2f100ed861481182fdb28d90ff517
janielledagondon <toonw48@gmail.com>
2023-07-03 11:06:21 +0800

    initialized view models

---
+++ app/Models/CourseEntries.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// View
+class CourseEntries extends Model
+{
+    use HasFactory;
+
+    protected $table = 'courseEntries';
+}
