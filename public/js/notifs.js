7cd3261e4ad8e346fb4aab9f9c1c3d26c101af9c
janielledagondon <toonw48@gmail.com>
2023-07-03 13:45:35 +0800

    toggle - success + created_at

--- public/js/notifs.js
+++ public/js/notifs.js
@@ -1,23 +1,56 @@
 function handleSSEUpdates() {
     const tbody = document.getElementById("notifs-tbody");
+    let toggleStates = [];
     let previousCreatedAt;
     let renderTimeout;
 
     function renderData(eventData) {
         const maxRows = eventData.length;
+        toggleStates = new Array(maxRows).fill(true); // Initialize toggleStates array with true values
 
         const rowsHtml = eventData
             .slice(0, maxRows)
-            .map((item) => `
+            .map((item, index) => `
                 <tr class="text-gray-900 border-b border-gray-300">
-                    <td class="px-4 py-2">${item.Phone}</td>
-                    <td class="px-4 py-2">${item.RequestType}</td>
+                    <td class="px-4 py-2" style="opacity: ${toggleStates[index] ? '1' : '0.5'}">${item.Phone}</td>
+                    <td class="px-4 py-2" style="opacity: ${toggleStates[index] ? '1' : '0.5'}">${item.RequestType}</td>
+                    <td class="px-4 py-2" style="opacity: ${toggleStates[index] ? '1' : '0.5'}">${item.created_at}</td>
+                    <td class="px-4 py-2 text-center">
+                        <i class="eye-icon fa-solid ${toggleStates[index] ? 'fa-eye' : 'fa-eye-slash'}"></i>
+                    </td>
                 </tr>
             `)
             .join('');
 
         tbody.innerHTML = rowsHtml;
+
+        // Add click event listener to the eye icons
+        const eyeIcons = document.querySelectorAll(".eye-icon");
+        eyeIcons.forEach((icon, index) => {
+            icon.addEventListener("click", () => handleEyeIconClick(index));
+        });
     }
+
+    function handleEyeIconClick(rowIndex) {
+        toggleStates[rowIndex] = !toggleStates[rowIndex]; // Toggle the state
+    
+        // Get the row elements
+        const rows = tbody.getElementsByTagName("tr");
+        const rowElements = Array.from(rows);
+    
+        // Get the corresponding td elements for the clicked row
+        const tds = rowElements[rowIndex].getElementsByTagName("td");
+    
+        // Change opacity of td elements based on the toggle state
+        tds[0].style.opacity = toggleStates[rowIndex] ? "1" : "0.5";
+        tds[1].style.opacity = toggleStates[rowIndex] ? "1" : "0.5";
+        tds[2].style.opacity = toggleStates[rowIndex] ? "1" : "0.5";
+    
+        // Change the eye icon based on the toggle state
+        const eyeIcon = rowElements[rowIndex].querySelector(".eye-icon");
+        eyeIcon.classList.remove(toggleStates[rowIndex] ? "fa-eye-slash" : "fa-eye");
+        eyeIcon.classList.add(toggleStates[rowIndex] ? "fa-eye" : "fa-eye-slash");
+    }    
 
     function handleSSEMessage(event) {
         const eventData = JSON.parse(event.data);
@@ -41,6 +74,7 @@ function handleSSEUpdates() {
             renderData(eventData);
         }, 100);
     }
+
     const eventSource = new EventSource("/sse-request");
     eventSource.onmessage = handleSSEMessage;
 }
--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -2,12 +2,13 @@
     <script src="{{ asset('js/notifs.js') }}"></script>
 
     <div class="w-full p-8 mb-4" id="notifs-container">
-        <div class="max-h-96 overflow-auto" style="width: 530px;">
+        <div class="max-h-96 overflow-auto w-fit">
             <table class="table-fixed sm:table-auto shadow-lg bg-white" id="notifs-table">
                 <thead class="sticky top-0 font-bold leading-6 tracking-widest text-left text-white bg-gray-800 border-b border-gray-200">
                     <tr>
                         <th class="p-4">Contact Number</th>
                         <th class="p-4">Request Type</th>
+                        <th class="p-4">Request Created</th>
                         <th class="p-4">Show/Hide</th>
                     </tr>
                 </thead>
