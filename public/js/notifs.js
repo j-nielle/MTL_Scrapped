d19da51ba58284ac93467b3f1be774ff9d7e7504
janielledagondon <toonw48@gmail.com>
2023-07-03 18:52:18 +0800

    v3

--- public/js/notifs.js
+++ public/js/notifs.js
@@ -4,23 +4,36 @@ function handleSSEUpdates() {
     let previousCreatedAt;
     let renderTimeout;
 
+    function handleSSEMessage(event) {
+        const eventData = JSON.parse(event.data);
+
+        if (eventData.length === 0) {
+            return;
+        }
+
+        const latestCreatedAt = eventData[0].created_at;
+
+        if (latestCreatedAt === previousCreatedAt) {
+            return;
+        } else {
+            previousCreatedAt = latestCreatedAt;
+            console.log("new");
+        }
+
+        clearTimeout(renderTimeout);
+        renderTimeout = setTimeout(() => {
+            renderData(eventData);
+        }, 100);
+    }
+
     function renderData(eventData) {
         const maxRows = eventData.length;
         toggleStates = new Array(maxRows).fill(true);
 
         const rowsHtml = eventData
             .slice(0, maxRows)
-            .map((item, index) => `
-            <tr class="text-gray-900 border-b border-gray-300">
-                <td class="px-4 py-2" style="opacity: ${toggleStates[index] ? '1' : '0.5'}">${item.Phone}</td>
-                <td class="px-4 py-2" style="opacity: ${toggleStates[index] ? '1' : '0.5'}">${item.RequestType}</td>
-                <td class="px-4 py-2" style="opacity: ${toggleStates[index] ? '1' : '0.5'}">${item.created_at}</td>
-                <td class="px-4 py-2 text-center toggle-row" style="cursor:pointer;">
-                    <i class="phone-icon fa-solid ${toggleStates[index] ? 'fa-phone' : 'fa-phone-slash'}"></i>
-                </td>
-            </tr>
-        `)
-            .join('');
+            .map((item, index) => createRowHtml(item, index))
+            .join("");
 
         tbody.innerHTML = rowsHtml;
 
@@ -28,6 +41,29 @@ function handleSSEUpdates() {
         toggleRows.forEach((row, index) => {
             row.addEventListener("click", () => handleToggleRowClick(index));
         });
+    }
+
+    function createRowHtml(item, index) {
+        const createdDate = new Date(item.created_at);
+        const formattedDate = createdDate.toLocaleString("en-US", {
+            month: "2-digit",
+            day: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+
+        const opacity = toggleStates[index] ? "1" : "0.5";
+        const phoneIconClass = toggleStates[index] ? "fa-phone" : "fa-phone-slash";
+
+        return `<tr class="text-gray-900 border-b border-gray-300">
+        <td class="px-4 py-2" style="opacity: ${opacity}">${item.Phone}</td>
+        <td class="px-4 py-2" style="opacity: ${opacity}">${item.RequestType}</td>
+        <td class="px-4 py-2" style="opacity: ${opacity}">${formattedDate}</td>
+        <td class="px-4 py-2 text-center toggle-row" style="cursor:pointer;">
+          <i class="phone-icon fa-solid ${phoneIconClass}"></i>
+        </td>
+      </tr>`;
     }
 
     function handleToggleRowClick(rowIndex) {
@@ -47,37 +83,8 @@ function handleSSEUpdates() {
         phoneIcon.classList.add(toggleStates[rowIndex] ? "fa-phone" : "fa-phone-slash");
     }
 
-    function handleSSEMessage(event) {
-        const eventData = JSON.parse(event.data);
-
-        if (eventData.length === 0) {
-            return;
-        }
-
-        const latestCreatedAt = eventData[0].created_at;
-        const date = new Date(latestCreatedAt);
-        const formattedDate = date.toLocaleString('en-US', {
-            month: '2-digit',
-            day: '2-digit',
-            year: 'numeric'
-        });
-
-        if (latestCreatedAt === previousCreatedAt) {
-            console.log('same timestamp');
-            return;
-        } else {
-            previousCreatedAt = latestCreatedAt;
-            console.log('new timestamp');
-        }
-
-        clearTimeout(renderTimeout);
-        renderTimeout = setTimeout(() => {
-            renderData(eventData);
-        }, 100);
-    }
-
     const eventSource = new EventSource("/sse-request");
     eventSource.onmessage = handleSSEMessage;
 }
 
-document.addEventListener('DOMContentLoaded', handleSSEUpdates);
+document.addEventListener("DOMContentLoaded", handleSSEUpdates);
